65f39284cca14267bcfdbb8e0114b1166f6bfa76
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-18 08:46:13 -0300

    feat: create react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './css/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div></div>
+    </>
+  )
+}
+
+export default App
